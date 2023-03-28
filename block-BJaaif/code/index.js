e894c1f34e2218640d9886844a855ef9377cccc3
wasim124 <mailofwasim@gmail.com>
2023-03-29 02:59:04 +0530

    complete

--- block-BJaaif/code/index.js
+++ block-BJaaif/code/index.js
@@ -156,20 +156,23 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore /* Put code here */;
+var adminFunctionStore = Object.create(userFunctionStore);/* Put code here */
 
 function adminFactory(name, score) {
   // Put code here
-  let admin = Object.create(userFactory(name, score));
+  let admin = userFactory(name, score);
+  Object.setPrototypeOf(admin,adminFunctionStore);
   admin.type = 'Admin';
   return admin;
 
 }
 
 /* Put code here for a method called sharePublicMessage*/
-
+adminFunctionStore.sharePublicMessage = function(){
+  console.log(`welcome user!`);
+}
 var adminFromFactory = adminFactory('Eva', 5);
 
 // /********* Uncomment these lines to test your work! *********/
-// adminFromFactory.sayType() // -> Logs "I am a Admin"
-// adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
+adminFromFactory.sayType() // -> Logs "I am a Admin"
+adminFromFactory.sharePublicMessage() // -> Logs "Welcome users!"
